55d9a29a1bdc1d9377bff06fd20d1c8637b78c5b
robertfeo <rofeit00@hs-esslingen.de>
2022-04-28 04:16:40 +0200

    update

--- src/main/java/com/example/restaurantorders/Controller.java
+++ src/main/java/com/example/restaurantorders/Controller.java
@@ -1,28 +1,46 @@
 package com.example.restaurantorders;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
+    private Label lbStatus;
+    @FXML
+    private Button btnQuit;
+    @FXML
+    private Button btnDbDisconnect;
+    @FXML
+    private Button btnDbConnect;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void connectToDatabase() throws IOException,NullPointerException {
+        lbStatus.setTextFill(Color.color(0, 0.5, 0.2));
+        lbStatus.setText("Erfolgreich verbunden mit der Datenbank.");
+        btnDbConnect.setDisable(true);
+        btnDbDisconnect.setDisable(false);
     }
 
     @FXML
-    protected void connectToDatabase() throws IOException {
-        System.out.println("connect to database");
+    protected void disconnectFromDatabase() throws IOException,NullPointerException {
+        lbStatus.setTextFill(Color.color(0.7, 0.1, 0.1));
+        lbStatus.setText("Verbindung wurde getrennt.");
+        btnDbDisconnect.setDisable(true);
+        btnDbConnect.setDisable(false);
     }
 
     @FXML
-    protected void disconnectFromDatabase() throws IOException {
-        System.out.println("disconnect from database");
+    protected void quitProg() throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-view.fxml"));
+        Stage stage = (Stage) btnQuit.getScene().getWindow();
+        stage.close();
+        System.exit(0);
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,13 @@
 module com.example.restaurantorders {
     requires javafx.controls;
     requires javafx.fxml;
-        requires javafx.web;
-
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            //requires validatorfx;
-            requires org.kordamp.ikonli.javafx;
-            requires org.kordamp.bootstrapfx.core;
-            requires eu.hansolo.tilesfx;
-
+    requires javafx.web;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.microsoft.sqlserver.jdbc;
     exports com.example.restaurantorders;
     opens com.example.restaurantorders to javafx.fxml;
 }
